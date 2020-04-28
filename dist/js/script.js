6cae99615468fdaa2b082caa423b5f51265307d7
Ivan <inftsukuyomi@yahoo.com>
2020-04-28 12:19:50 +0300

    no adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,5 +21,66 @@ $(document).ready(function(){
             }
         ]
       });
+    // modal
+      $('.modal__close').on('click', function(){
+        $('.overlay, #thanks').fadeOut('slow');
+    });
+
+      $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1000){
+          $('.up').fadeIn();
+        } else {
+          $('.up').fadeOut();
+        }
+    });
+
+    $('input[name=phone]').mask("+7(999)999-99-99");
+
+    function validateForms(form){
+        $(form).validate({
+          rules: {
+            name: "required",
+            text: "required",
+            phone: "required",
+            email: {
+              required: true,
+              email: true 
+            }
+          },
+          messages: {
+            text: "Пожалйуста, введите своё сообщение",
+            name: "Пожалуйста, введите своё имя",
+            phone: "Пожалуйста, введите свой номер",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильный формат почты"
+            }
+          }
+        });
+    };
+  
+    validateForms('#consultation-form')
+    validateForms('#consultation form')
+    validateForms('#order form')
     
+    $('form').submit(function(e){
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function(){
+          $(this).find("input").val("");
+          $('.overlay, #thanks').fadeIn('fast');
+          $('form').trigger('reset');
+      });
+      return false;
+    });
+    new WOW().init();
   });
